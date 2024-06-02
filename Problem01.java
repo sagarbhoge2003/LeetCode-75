139ebf38a052ffd0761890abc8aad543efabdf5b
Sagar Bhoge <115706182+sagarbhoge2003@users.noreply.github.com>
2024-06-02 22:30:22 +0530

    Update Problem01.java

--- Problem01.java
+++ Problem01.java
@@ -1,5 +1,5 @@
 //1768. Merge Strings Alternately
-
+//https://leetcode.com/problems/merge-strings-alternately/?envType=study-plan-v2&envId=leetcode-75
 import java.util.*;
 class Problem01 {
     public String mergeAlternately(String word1, String word2) 
