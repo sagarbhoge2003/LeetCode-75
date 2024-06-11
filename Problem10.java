b87ec4ce3978f712002c90b85b70992029690002
Sagar Bhoge <115706182+sagarbhoge2003@users.noreply.github.com>
2024-06-11 21:56:55 +0530

    Add files via upload

---
+++ Problem10.java
@@ -0,0 +1,17 @@
+//283. Move Zeroes
+//https://leetcode.com/problems/move-zeroes/description/?envType=study-plan-v2&envId=leetcode-75
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int snowBallSize = 0; 
+       for (int i=0;i<nums.length;i++){
+           if (nums[i]==0){
+               snowBallSize++; 
+           }
+           else if (snowBallSize > 0) {
+               int t = nums[i];
+               nums[i]=0;
+               nums[i-snowBallSize]=t;
+           }
+       }
+   }
+}
